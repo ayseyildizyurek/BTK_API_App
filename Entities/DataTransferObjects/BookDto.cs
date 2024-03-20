b5179c3f3e3a5a17472327356e695c4dd561df8a
Ayşe YILDIZ YÜREK <ayseyildizyurek@outlook.com>
2024-03-20 09:54:30 +0300

    Output formats configured

---
+++ Entities/DataTransferObjects/BookDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities.DataTransferObjects
+{
+	/*[Serializable]*/ //Serileştirilebilir, örneğin xml formatında maplemede 500 hatası dönüyordu çözüm olarak bu attribute gerekli
+	//public record BookDto(int Id, String Title, decimal Price); //Çıktı backfieldlarla geldiği için aşağıdaki tarzda yazdık
+	public record BookDto
+	{
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public decimal Price { get; set; }
+    }
+	
+}
---
+++ WebAPI/Extensions/IMvcBuilderExtensions.cs
@@ -0,0 +1,9 @@
+﻿using WebAPI.Utilities.Formatters;
+
+namespace WebAPI.Extensions
+{
+	public static class IMvcBuilderExtensions
+	{
+		public static IMvcBuilder AddCustomCsvFormatter(this IMvcBuilder builder) => builder.AddMvcOptions(config => config.OutputFormatters.Add(new CscOutputFormatter()));
+	}
+}
